7de69bc24d21f158104e2e180a8934c9466d653a
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-09-27 17:43:41 -0300

    Refatoração do RabbitMQ Bus e criação do método que grava uma transferência.

--- MicroRabbit/MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
+++ MicroRabbit/MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -14,6 +14,12 @@ namespace MicroRabbit.Transfer.Data.Repository
             _context = context;
         }
 
+        public void Add(TransferLog transferLog)
+        {
+            _context.TransferLogs.Add(transferLog);
+            _context.SaveChanges();
+        }
+
         public IEnumerable<TransferLog> GetTransferLogs()
         {
             return _context.TransferLogs;
