66f92f409c4294958fce773f79967eeff5a18ad2
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-09-27 18:32:30 -0300

    Implementação do projeto MVC que comunica com o microserviço de Banking.

---
+++ MicroRabbit/MicroRabbit.MVC/Services/ITransferService.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+}
---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _client;
+
+        public TransferService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), 
+                                                    Encoding.UTF8, 
+                                                    "application/json");
+
+            var response = await _client.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
